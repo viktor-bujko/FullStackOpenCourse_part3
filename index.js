c945b1124c55990d75d51f8b6032b7af308b97a7
Viktor Bujko <viktorbjk@gmail.com>
2021-11-07 16:49:47 +0100

    Deploy

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const Person = require('./modules/person')
 
 morgan.token('body', (request, _) => JSON.stringify(request.body))
 
+app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 app.post(
